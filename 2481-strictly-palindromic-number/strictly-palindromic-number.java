f551bb94e7f8bf8a5d6828d162f578a948382bf1
Kaushik Balasubramanian <149362832+KAUSHIK21AID@users.noreply.github.com>
2024-05-08 21:13:05 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (65.63%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.java
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.java
@@ -1,5 +1,30 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        boolean flag = false;
+
+        for(int i=2;i<n-2;i++)
+        {
+            if(!ispalindrome(Integer.toString(n,i)))
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        return flag;
+    }
+    public boolean ispalindrome(String s)
+    {
+        int left=0, right=s.length()-1;
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
