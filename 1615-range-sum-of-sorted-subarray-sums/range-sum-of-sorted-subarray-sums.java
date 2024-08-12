ccc7e882c921e3e75df0db233996710ff2b4947a
Kaushik Balasubramanian <149362832+KAUSHIK21AID@users.noreply.github.com>
2024-08-13 00:30:35 +0530

    Time: 181 ms (17.54%) | Memory: 70.7 MB (18.30%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.java
@@ -1,22 +1,22 @@
 class Solution {
     public int rangeSum(int[] nums, int n, int left, int right) {
-        int sum = 0, ind=0;
-        int sums[] = new int[n*(n+1)/2];
+        int sum = 0;
+        List<Integer>sums = new ArrayList<>();
         for(int i=0;i<n;i++)
         {
             sum = nums[i];
-            sums[ind++] = sum;
+            sums.add(sum);
             for(int j=i+1;j<n;j++)
             {
                 sum = sum + nums[j];
-                sums[ind++] = sum;
+                sums.add(sum);
             }
         }
-        Arrays.sort(sums);
+        Collections.sort(sums);
         int anssum=0;
         for(int i=left-1;i<right;i++)
         {
-            anssum = (anssum + sums[i])%(int)(Math.pow(10,9)+7);
+            anssum = (anssum + sums.get(i))%(int)(Math.pow(10,9)+7);
         }
         return anssum;
         
